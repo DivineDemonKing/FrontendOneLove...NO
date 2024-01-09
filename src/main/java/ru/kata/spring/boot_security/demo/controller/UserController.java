db67cc9ca3fe2daff2ba2bbad8a79bc37e0c7d7f
George <mulgachev,junior@gmail.com>
2024-01-09 08:15:27 +0300

    Added admin controller

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -37,42 +37,4 @@ public class UserController {
 		model.addAttribute(user);
 		return "user";
 	}
-
-	@GetMapping(value = "/admin")
-	public String printUser(ModelMap model) {
-		List<User> users = userService.listUsers();
-		model.addAttribute("users", users);
-		return "userList";
-	}
-	@GetMapping(value = "/addUser")
-	public String newUser(ModelMap model) {
-		model.addAttribute("user", new User());
-		return "addUser";
-	}
-	@PostMapping(value = "/addUser")
-	public String createUser(ModelMap model, @ModelAttribute("user") User user) {
-		userService.add(user);
-		return "redirect:/admin";
-	}
-	@GetMapping(value = "/deleteUser")
-	public String getIdToDelete(ModelMap model) {
-		model.addAttribute("user", new User());
-		return "/deleteUser";
-	}
-	@PostMapping(value = "/deleteUser")
-	public String deleteUser(ModelMap model, @ModelAttribute("user") User user) {
-		userService.deleteById(user.getId());
-		return "redirect:/admin";
-	}
-
-	@GetMapping(value = "/updateUser")
-	public String getIdToUpdate(ModelMap model) {
-		model.addAttribute("user", new User());
-		return "/updateUser";
-	}
-	@PostMapping(value = "/updateUser")
-	public String updateUser(ModelMap model, @ModelAttribute("user") User user) {
-		userService.update(user);
-		return "redirect:/admin";
-	}
 }
